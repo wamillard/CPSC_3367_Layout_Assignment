26e07273084824a6f076e665c41522596cc9116a
wamillard <wamillard@yahoo.com>
2021-11-18 12:04:28 -0600

    finished chips, started Card 1

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ualr.layoutassignment.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
 
     // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
-
+    private ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
+
 }
