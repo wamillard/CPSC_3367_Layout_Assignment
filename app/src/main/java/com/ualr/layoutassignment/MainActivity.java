eac8f2bf6a75085a8dd86bae75164b7ae5a88821
wamillard <wamillard@yahoo.com>
2021-11-22 11:31:24 -0600

    Completed Assignment

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -133,126 +133,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return;
-/*
-        List<ConstraintLayout> layoutInvoice=buildInvoice(this);
 
-
-        for (Integer i=0;i<layoutInvoice.size();i++)
-        {
-            this.mBinding.layoutProductList.addView(layoutInvoice.get(i));
-        }
-
-
-        buildInvoice(this);
-*/
 
     
     }
 
-//    public List<ConstraintLayout> buildInvoice(Context ctx)
 
-/*
-    public void buildInvoice(Context ctx)
-    {
-
-        ctx=getBaseContext();
-
-        List<Product> newList=getList();
-        List<ConstraintLayout> newLayout=new ArrayList<>();
-
-
-        for(Integer i=0; i<newList.size();i++)
-        {
-            ConstraintLayout newLayer=new ConstraintLayout(ctx);
-            ConstraintSet constraintSet=new ConstraintSet();
-
-            newLayer.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-            CheckBox check=new CheckBox(new ContextThemeWrapper(ctx,R.style.CheckBox_Blue));
-            EditText qty=new EditText(new ContextThemeWrapper(ctx,R.style.EditText_Quantity));
-            MaterialButton sub=new MaterialButton(new ContextThemeWrapper(ctx,R.style.Button_Quantity_Subtract));
-            MaterialButton add=new MaterialButton(new ContextThemeWrapper(ctx,R.style.Button_Quantity_Add));
-            TextView cost=new TextView(new ContextThemeWrapper(ctx,R.style.TextView_Product));
-            TextView subtotal=new TextView(new ContextThemeWrapper(ctx,R.style.TextView_Product));
-
-            Integer newID=View.generateViewId();
-            check.setId(Integer.parseInt(i.toString()+newID.toString()));
-            check.setText(newList.get(i).getName());
-            check.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-
-            newID=View.generateViewId();
-            qty.setId(Integer.parseInt(i.toString()+newID.toString()));
-            qty.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-            newID=View.generateViewId();
-            sub.setId(Integer.parseInt(i.toString()+newID.toString()));
-            sub.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-            newID=View.generateViewId();
-            add.setId(Integer.parseInt(i.toString()+newID.toString()));
-            add.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-            newID=View.generateViewId();
-            cost.setId(Integer.parseInt(i.toString()+newID.toString()));
-            cost.setText("$  "+newList.get(i).getCost().toString());
-            cost.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-            newID=View.generateViewId();
-            subtotal.setId(Integer.parseInt(i.toString()+newID.toString()));
-            subtotal.setText("$   0.00");
-            subtotal.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-
-            constraintSet.connect(check.getId(),ConstraintSet.START,newLayer.getId(),ConstraintSet.START);
-            constraintSet.connect(check.getId(),ConstraintSet.TOP,newLayer.getId(),ConstraintSet.TOP);
-
-            constraintSet.connect(qty.getId(),ConstraintSet.START,sub.getId(),ConstraintSet.END);
-            constraintSet.connect(qty.getId(),ConstraintSet.TOP,newLayer.getId(),ConstraintSet.TOP);
-
-            constraintSet.connect(sub.getId(),ConstraintSet.TOP,newLayer.getId(),ConstraintSet.TOP);
-            constraintSet.connect(sub.getId(),ConstraintSet.START,check.getId(),ConstraintSet.END);
-            constraintSet.connect(sub.getId(),ConstraintSet.BOTTOM,newLayer.getId(),ConstraintSet.BOTTOM);
-
-            constraintSet.connect(add.getId(),ConstraintSet.TOP,newLayer.getId(),ConstraintSet.TOP);
-            constraintSet.connect(add.getId(),ConstraintSet.START,qty.getId(),ConstraintSet.END);
-            constraintSet.connect(add.getId(),ConstraintSet.BOTTOM,newLayer.getId(),ConstraintSet.BOTTOM);
-
-            constraintSet.connect(cost.getId(),ConstraintSet.TOP,newLayer.getId(),ConstraintSet.TOP);
-            constraintSet.connect(cost.getId(),ConstraintSet.END,subtotal.getId(),ConstraintSet.START);
-            constraintSet.connect(cost.getId(),ConstraintSet.BOTTOM,qty.getId(),ConstraintSet.BOTTOM);
-
-            constraintSet.connect(subtotal.getId(),ConstraintSet.TOP,newLayer.getId(),ConstraintSet.TOP);
-            constraintSet.connect(subtotal.getId(),ConstraintSet.START,newLayer.getId(),ConstraintSet.START);
-            constraintSet.connect(subtotal.getId(),ConstraintSet.BOTTOM,qty.getId(),ConstraintSet.BOTTOM);
-
-            add.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    quantityChange(true, v.getTag().toString());
-                }
-
-            });
-
-            newLayer.addView(check);
-            newLayer.addView(qty);
-            newLayer.addView(sub);
-            newLayer.addView(add);
-            newLayer.addView(cost);
-            newLayer.addView(subtotal);
-
-            constraintSet.applyTo(newLayer);
-
-//            newLayout.add(newLayer);
-
-            this.addContentView(newLayer, newLayer.getLayoutParams());
-        }
-
-
-//        return newLayout;
-    };
-*/
 
     public List<Product> getList()
     {
@@ -336,9 +222,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         this.mBinding.txtTotal.setText(String.format("%.2f",newTotal));
-
-
-
 
         return;
     }
